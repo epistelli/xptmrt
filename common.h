8b9fc413b6694cea70913e6d7f550f2e0bfd5ea0
daniel <daniel@icerbero.com>
2018-07-21 15:15:04 +0200

    winsock includes for the future

--- common.h
+++ common.h
@@ -1,8 +1,11 @@
 #ifndef _XPTMRT_COMMON_H
 #define _XPTMRT_COMMON_H
 
+#define _WINSOCKAPI_ 
 #include <Windows.h>
 #include <processthreadsapi.h>
+#include <ip2string.h>
+#include <Winsock2.h>
 #include <string.h>
 #include "ntdll.h"
 
